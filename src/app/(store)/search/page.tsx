b4e46027f4261138b648c41df3007f1d03965a87
FeAranha <felipearanha.c@gmail.com>
2024-02-14 10:55:02 -0300

    Home page

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -1,5 +1,12 @@
+import { Suspense } from 'react'
+import SearchLoading from './loading'
+
 export default async function Search() {
   await new Promise((resolve) => setTimeout(resolve, 3000))
 
-  return <h1>Search</h1>
+  return (
+    <Suspense fallback={<SearchLoading />}>
+      <h1>Search</h1>
+    </Suspense>
+  )
 }
