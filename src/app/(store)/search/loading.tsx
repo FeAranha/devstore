d42eee23ca7eb8af9a5a7e7dc2b8704dbad9890a
FeAranha <felipearanha.c@gmail.com>
2024-02-20 15:46:48 -0300

    Loading da busca

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,12 +1,27 @@
+'use client'
+
 import { Skeleton } from '@/components/skeleton'
+import { useSearchParams } from 'next/navigation'
 
 export default function SearchLoading() {
-  return (
-    <div className="grid h-full grid-cols-9 grid-rows-6 gap-6">
-      <Skeleton className="col-span-6 row-span-6 h-[856px]" />
+  const searchParams = useSearchParams()
 
-      <Skeleton className="col-span-3 row-span-3" />
-      <Skeleton className="col-span-3 row-span-3" />
+  const query = searchParams.get('q')
+
+  return (
+    <div className="flex flex-col gap-4">
+      <p className="text-sm">
+        Resultados para: <span className="font-semibold">{query}</span>
+      </p>
+
+      <div className="grid grid-cols-3 gap-6">
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+      </div>
     </div>
   )
 }
